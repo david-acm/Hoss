76b791c908fc0e4b85239f3611d2dbf4c7bb8bb9
davidchaparro <davidacm7@outlook.com>
2023-09-27 00:15:37 -0400

    Adds integration for profile created

--- Bookings/Application/BookingsCommandService.cs
+++ Bookings/Application/BookingsCommandService.cs
@@ -7,28 +7,28 @@ using static Bookings.Application.BookingCommands;
 namespace Bookings.Application;
 
 public class BookingsCommandService : CommandService<Booking, BookingState, BookingId> {
-    public BookingsCommandService(IAggregateStore store, Services.IsRoomAvailable isRoomAvailable) : base(store) {
-        OnNewAsync<BookRoom>(
-            cmd => new BookingId(cmd.BookingId),
-            (booking, cmd, _) => booking.BookRoom(
-                cmd.GuestId,
-                new RoomId(cmd.RoomId),
-                new StayPeriod(LocalDate.FromDateTime(cmd.CheckInDate), LocalDate.FromDateTime(cmd.CheckOutDate)),
-                new Money(cmd.BookingPrice, cmd.Currency),
-                new Money(cmd.PrepaidAmount, cmd.Currency),
-                DateTimeOffset.Now,
-                isRoomAvailable
-            )
-        );
+  public BookingsCommandService(IAggregateStore store, Services.IsRoomAvailable isRoomAvailable) : base(store) {
+    OnNewAsync<BookRoom>(
+      cmd => new BookingId(cmd.BookingId),
+      (booking, cmd, _) => booking.BookRoom(
+        cmd.GuestId,
+        new RoomId(cmd.RoomId),
+        new StayPeriod(LocalDate.FromDateTime(cmd.CheckInDate), LocalDate.FromDateTime(cmd.CheckOutDate)),
+        new Money(cmd.BookingPrice, cmd.Currency),
+        new Money(cmd.PrepaidAmount, cmd.Currency),
+        DateTimeOffset.Now,
+        isRoomAvailable
+      )
+    );
 
-        OnExisting<RecordPayment>(
-            cmd => new BookingId(cmd.BookingId),
-            (booking, cmd) => booking.RecordPayment(
-                new Money(cmd.PaidAmount, cmd.Currency),
-                cmd.PaymentId,
-                cmd.PaidBy,
-                DateTimeOffset.Now
-            )
-        );
-    }
+    OnExisting<RecordPayment>(
+      cmd => new BookingId(cmd.BookingId),
+      (booking, cmd) => booking.RecordPayment(
+        new Money(cmd.PaidAmount, cmd.Currency),
+        cmd.PaymentId,
+        cmd.PaidBy,
+        DateTimeOffset.Now
+      )
+    );
+  }
 }
